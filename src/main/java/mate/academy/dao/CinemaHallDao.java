547402d0aefeeaf1058bd0068b74f99d0ee279a6
Woterix <dimkabibimka333@gmail.com>
2025-11-15 16:00:24 +0000

    HomeWork6

--- src/main/java/mate/academy/dao/CinemaHallDao.java
+++ src/main/java/mate/academy/dao/CinemaHallDao.java
@@ -5,9 +5,9 @@ import java.util.Optional;
 import mate.academy.model.CinemaHall;
 
 public interface CinemaHallDao {
-    CinemaHall add(CinemaHall cinemaHall);
-
     Optional<CinemaHall> get(Long id);
+
+    CinemaHall add(CinemaHall cinemaHall);
 
     List<CinemaHall> getAll();
 }
